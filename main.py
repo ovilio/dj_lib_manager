bb4dc054742342d115bde6c930d045c183576250
volodymyr.dodoka <volodymyr.dodoka@pm.bet>
2021-12-26 21:39:43 +0200

    addded main method

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import csv
 import os
+import click
 from Track import Track
 from tinytag import TinyTag
 
 tracksDic = {}
 
-# open csv
-with open('./resources/playlist.csv') as csv_file:
-    csvReader = csv.reader(csv_file)
-    next(csvReader)
-    for csvLine in csvReader:
-        # fill dictionary of tracks with tracks from csv (key = trackname)
-        track = Track(csvLine[0], csvLine[1], csvLine[2])
-        tracksDic[track.title] = track
 
-print('List of tracks titles from csv')
-for elem in tracksDic:
-    print(elem)
+def main():
+    # open csv
+    with open('./resources/playlist.csv') as csv_file:
+        csvReader = csv.reader(csv_file)
+        next(csvReader)  # to skip the header
+        for csvLine in csvReader:
+            # fill dictionary of tracks with tracks from csv (key = trackname)
+            track = Track(csvLine[0], csvLine[1], csvLine[2])
+            tracksDic[track.title] = track
 
-# get list of filenames in current directory
-filesInDirList = filter(lambda x: x.endswith('.mp3') | x.endswith('.flac'), os.listdir())
+    print('List of tracks titles from csv')
+    for elem in tracksDic:
+        print(elem)
 
-for fileName in filesInDirList:
-    # get metadata from each file
-    fileMetaData = TinyTag.get(fileName)
-    print('Trying to found csv line by track title: ' + fileMetaData.title.split()[0])
-    foundTrack = tracksDic.get(fileMetaData.title.split()[0])
-    print('found: ' + str(foundTrack))
-    if foundTrack is not None:
-        # mark track as downloaded and put into dictionary
-        foundTrack.is_downloaded = True
-        tracksDic.update({foundTrack.title: foundTrack})
+    # get list of filenames in current directory
+    filesInDirList = filter(lambda x: x.endswith('.mp3') | x.endswith('.flac'), os.listdir())
 
-print('List of tracks titles from csv')
-for elem in tracksDic.values():
-    print(elem)
+    for fileName in filesInDirList:
+        # get metadata from each file
+        fileMetaData = TinyTag.get(fileName)
+        print('Trying to found csv line by track title: ' + fileMetaData.title.split()[0])
+        foundTrack = tracksDic.get(fileMetaData.title.split()[0])
+        print('found: ' + str(foundTrack))
+        if foundTrack is not None:
+            # mark track as downloaded and put into dictionary
+            foundTrack.is_downloaded = True
+            tracksDic.update({foundTrack.title: foundTrack})
+
+    print('List of tracks titles from csv')
+    for elem in tracksDic.values():
+        print(elem)
+
+
+if __name__ == '__main__':
+    main()
